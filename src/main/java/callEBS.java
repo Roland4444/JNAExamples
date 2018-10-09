f195f488b358ca1ebe92cda07a23759c60289d8f
Roland4444 <turbopollux@rambler.ru>
2018-10-09 16:38:01 +0000

    NEED TEST

---
+++ src/main/java/callEBS.java
@@ -0,0 +1,35 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class callEBS {
+    public interface CLibrary extends Library {
+        public static class ResultCheck extends Structure {
+            @Override
+            protected List<String> getFieldOrder() {
+                return Arrays.asList(new String[] { "check", "proc_return","incallreturn" });
+
+            }
+            public static class ByReference extends CLibrary.ResultCheck implements Structure.ByReference {}
+            public int check;
+            public int proc_return;
+            public int incallreturn;
+
+        }
+
+
+        callCplplStruct.CLibrary INSTANCE = (callCplplStruct.CLibrary) Native.loadLibrary(("simpleSo"), callCplplStruct.CLibrary.class);
+        ResultCheck lets_check(String config, String filename);
+
+    }
+    CLibrary.ResultCheck call_ebs(String config, String filename){
+        return CLibrary.INSTANCE.lets_check( config,  filename);
+    }
+
+    callCplplStruct.CLibrary.resultStruct callLetsSumm(callCplplStruct.CLibrary.inputStruct flow){
+        return callCplplStruct.CLibrary.INSTANCE.letsSumm(flow);
+    }
+}
